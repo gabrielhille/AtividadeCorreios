77d94fb17af9db3de73d81eeb8c7404e31614805
jeffreire0711@gmail.com <jeffreire0711@gmail.com>
2019-10-23 15:06:19 -0300

    comitti

--- AtividadeCorreios/AtividadeCorreios/Scripts/Utils/Script.js
+++ AtividadeCorreios/AtividadeCorreios/Scripts/Utils/Script.js
@@ -1,40 +1,34 @@
 ﻿$(document).ready(function () {
     $('input[name="BTN"]').click(function () {
 
-      var codigo = $('input[name="codigo"]').val(data.codigo);
-      var valor = $('input[name="valor"]').val(data.valor);
-      var prazoentrega = $('input[name="prazoentrega"]').val(data.prazoentrega);
-      var cepori = $('input[name="cepori"]').val(data.localidade);
-      var cepdes = $('input[name="cepdes"]').val(data.uf);
-      var peso = $('input[name="peso"]').val(data.unidade);
-      var comprimento = $('input[name="comprimento"]').val(data.ibge);
-      var formato = $('input[name="formato"]').val(data.gia);
-      var altura = $('input[name="altura"]').val(data.gia);
-      var larfura = $('input[name="largura"]').val(data.gia);
-      var diametro = $('input[name="diametro"]').val(data.gia);
-      var maopropria = $('input[name="maopropria"]').val(data.gia);
-      var valordeclarado = $('input[name="valordeclarado"]').val(data.gia);
-      var avisorecebimento = $('input[name="avisorecebimento"]').val(data.gia);
+        var codigo = $('input[name="codigo"]').val();
+        var valor = $('input[name="valor"]').val();
+        var prazoentrega = $('input[name="prazoentrega"]').val();
+        var cepori = $('input[name="cepori"]').val();
+        var cepdes = $('input[name="cepdes"]').val();
+        var peso = $('input[name="peso"]').val();
+        var comprimento = $('input[name="comprimento"]').val();
+        var formato = $('input[name="formato"]').val();
+        var altura = $('input[name="altura"]').val();
+        var largura = $('input[name="largura"]').val();
+        var diametro = $('input[name="diametro"]').val();
+        var maopropria = $('input[name="maopropria"]').val();
+        var valordeclarado = $('input[name="valordeclarado"]').val();
+        var avisorecebimento = $('input[name="avisorecebimento"]').val();
 
 
-        $.get("http://usysweb.com.br/api/correiosambev.php?nCdServico=04014", function (data) {
-
-            $('input[name="codigo"]').val(data.codigo);
-            $('input[name="valor"]').val(data.valor);
-            $('input[name="prazoentrega"]').val(data.prazoentrega);
-            $('input[name="cepori"]').val(data.cepori);
-            $('input[name="cepdes"]').val(data.cepdes);
-            $('input[name="peso"]').val(data.peso);
-            $('input[name="comprimento"]').val(data.comprimento);
-            $('input[name="formato"]').val(data.formato);
-            $('input[name="altura"]').val(data.altura);
-            $('input[name="largura"]').val(data.largura);
-            $('input[name="diametro"]').val(data.diametro);
-            $('input[name="maopropria"]').val(data.maopropria);
-            $('input[name="valordeclarado"]').val(data.valordeclarado);
-            $('input[name="avisorecebimento"]').val(data.avisorecebimento);
+        $.get("http://usysweb.com.br/api/correiosambev.php?" + codigo + valor + prazoentrega + cepori + cepdes + peso + comprimento + formato + altura + largura + diametro + maopropria + valordeclarado + avisorecebimento, function (data) {
 
 
-        });
-    });
-});
+                  
+                  
+        });       
+    });           
+});               
+                  
+                  
+                  
+                  
+                  
+                  
+                  
