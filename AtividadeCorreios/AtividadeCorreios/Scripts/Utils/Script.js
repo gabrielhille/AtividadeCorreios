533d2a07301cc8bfda7ef45d11da4432f9172a38
jeffreire0711@gmail.com <jeffreire0711@gmail.com>
2019-10-23 09:55:14 -0300

    Commit mostro Segundo

--- AtividadeCorreios/AtividadeCorreios/Scripts/Utils/Script.js
+++ AtividadeCorreios/AtividadeCorreios/Scripts/Utils/Script.js
@@ -1,1 +1,23 @@
-﻿asd
+﻿$(document).ready(function () {
+    $('input[name="bntconsultar"]').click(function () {
+        var cep = $('input[name="cep"]').val();
+
+        
+        $.get("http://usysweb.com.br/api/correiosambev.php?" + cep + "/json/", function (data) {
+
+            if (data.erro) {
+                alert("Cep inválido!");
+                return false;
+            }
+
+            $('input[name="codigo"]').val(data.codigo);
+            $('input[name="valor"]').val(data.valor);
+            $('input[name="prazoentrega"]').val(data.prazoentrega);
+            $('input[name="cepori"]').val(data.localidade);
+            $('input[name="uf"]').val(data.uf);
+            $('input[name="unidade"]').val(data.unidade);
+            $('input[name="ibge"]').val(data.ibge);
+            $('input[name="gia"]').val(data.gia);
+        });
+    });
+});
